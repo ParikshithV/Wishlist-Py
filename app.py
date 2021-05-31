61db9dae860fb4dabdc55eda88a4b088cf16b1e4
Parikshith <parikv99@gmail.com>
2021-06-01 03:05:32 +0530

    bknd functional

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
 tz = pytz.timezone('Asia/Kolkata')
 datetime_ind = datetime.now(tz)
 
@@ -13,8 +18,8 @@ class Wishlist(db.Model):
     name = db.Column(db.String(250), primary_key=True)
     link = db.Column(db.String(250))    #add nullable=False
     site_name = db.Column(db.String(100),)
-    image = db.Column(db.String(250),)
-    price = db.Column(db.Integer, default=0)
+    image = db.Column(db.Unicode)
+    price = db.Column(db.String(250), default=0)
     date_added = db.Column(db.DateTime, default=datetime_ind)
 
     def __repr__(self):
@@ -24,14 +29,22 @@ class Wishlist(db.Model):
 def index():
     if request.method == 'POST':
         item_name = request.form['content']
-        new_item = Wishlist(name=item_name)
 
-        try:
-            db.session.add(new_item)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'Error adding item'
+        # try:
+        req = requests.get(item_name)
+        soup = BeautifulSoup(req.content,'html.parser')
+        price = soup.find('span',id="our_price_display").string
+        name = soup.find('h2', class_ = "product-name").string
+        image = soup.find_all('img')
+        image_link = image[2]['src']
+        time.sleep(3)
+        new_item = Wishlist(name=name, link=item_name, site_name='SSS', image=image_link, price=price)
+
+        db.session.add(new_item)
+        db.session.commit()
+        return redirect('/')
+        # except:
+        #     return 'Error adding item'
     
     else:
         items = Wishlist.query.order_by(Wishlist.date_added).all()
