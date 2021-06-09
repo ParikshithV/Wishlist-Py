3543b0891955c205c60baf4c364835fd42ca7f42
Parikshith <parikv99@gmail.com>
2021-06-10 01:42:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask, render_template, url_for, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, insert, Unicode, DateTime
